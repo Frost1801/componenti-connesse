78b91e4674deb35cfe8f80a03c009a5ad9e1261f
Frost1801 <sergio.cibecchini@gmail.com>
2023-02-15 12:42:26 +0100

    completed random graph

--- RandomGraph.py
+++ RandomGraph.py
@@ -1,14 +1,29 @@
 from random import randrange
 
 
-class Graph:
+class RandomGraph:
     # n è il numero di nodi del grafo
-    def __int__(self, n):
+    def __init__(self, n):
         if n <= 0:
             raise (ValueError("Vertex value must be at least 1"))  # controlliamo che il numero di vertici sia almeno 1
-        self.nV = n  # numero di vertici
-        self.v = {}  # set dei vertici
+        self.numVert = n  # numero di vertici
+        self.vertices = {}  # set dei vertici
         for i in range(0, n):
-            self.v[i] = (str(i))  # riempie il set dei vertici
-        maxEdges = (n * (n - 1)) / 2  # numero massimo di archi in un grafo di n elementi
-        self.nE = randrange(0, maxEdges)  # genera il numero di archi presenti
+            self.vertices[i] = (str(i))  # riempie il set dei vertici
+        maxEdges = int((n * (n - 1)) / 2)  # numero massimo di archi in un grafo di n elementi
+        numEdges = randrange(0, maxEdges)  # genera il numero di archi presenti
+        self.edges = {}  # set di tuple
+        self.adjacencyList = {}  # dichiariamo la lista di adiacenza
+        for i in range(0, self.numVert):
+            self.adjacencyList[i] = []  # inizializza la lista di adiacenza
+        for i in range(0, numEdges):
+            while True:  # crea due indici diversi
+                v1 = randrange(0, self.numVert)
+                v2 = randrange(0, self.numVert)
+                if v1 != v2:
+                    break
+            if (v1, v2) not in self.edges.values() and (v2, v1) not in self.edges.values():
+                self.edges[i] = (v1, v2)
+                self.adjacencyList[v1].append(v2)  # riempie la lsita di adiacenza
+                self.adjacencyList[v2].append(v1)
+        self.numEdges = len(self.edges)
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+#TODO IMPLEMENTAZIONE GRAFO
+    #creiamo prima i grafi per gli n valori e li memorizziamo in modo da non sprecare tempo per la creazione
+#TODO CLASSE BASE UNION FIND
 
+    #TODO LISTA BASE
+    #TODO LISTA CON EURISTICA
+    #TODO ALBERI
+
+#TODO SALVATAGGIO SU FILE
+
+from RandomGraph import RandomGraph
 
 if __name__ == '__main__':
-    print()
+    n = 5
+    rg = RandomGraph(n)
+    print(rg)
