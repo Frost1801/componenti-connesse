88c0e4187da4621f2c5567765c1189d31cee7647
Frost1801 <sergio.cibecchini@gmail.com>
2023-02-16 11:01:06 +0100

    Implemented some simple test functions to test the results

--- LinkedList.py
+++ LinkedList.py
@@ -1,37 +1,32 @@
 from typing import Optional
 
-from DisjointSet import DisjointSet
-from RandomGraph import RandomGraph
-
-#rappresenta un nodo della lista
+# rappresenta un nodo della lista
 
 class ListNode:
     def __init__(self, data: Optional[chr], nextNode):
         self.val = data
         self.next = nextNode
-        self.list = None  #mantiene il valore del set object (LinkedList) del nodo
+        self.list = None  # mantiene il valore del set object (LinkedList) del nodo
 
 
 class LinkedList:  # set object
     def __init__(self, node: Optional[ListNode]):
         self.head = node
         self.tail = node
-        node.list = self  #aggiorna il valore del set object del nodo
+        node.list = self  # aggiorna il valore del set object del nodo
 
-    #stampa la lista
+    # stampa la lista
     def print(self):
         node = self.head
         while node:
-            print(node.val)
+            if node.next:
+                print(node.val, end=" - ")
+            else:
+                print(node.val)
             node = node.next
-        print()
 
-class ExtendedLinkedList (LinkedList):
+
+class ExtendedLinkedList(LinkedList):
     def __init__(self, node: Optional[ListNode]):
         super().__init__(node)
-        self.length = 1 #siccome la lista inizia con un singolo elemento, la lunghezza è inizializzata ad 1
-
-
-
-
-
+        self.length = 1  # siccome la lista inizia con un singolo elemento, la lunghezza è inizializzata ad 1
--- main.py
+++ main.py
@@ -1,27 +1,52 @@
-#TODO IMPLEMENTAZIONE GRAFO
-    #creiamo prima i grafi per gli n valori e li memorizziamo in modo da non sprecare tempo per la creazione
-#TODO CLASSE BASE UNION FIND
+# TODO IMPLEMENTAZIONE GRAFO
+# creiamo prima i grafi per gli n valori e li memorizziamo in modo da non sprecare tempo per la creazione
+# TODO CLASSE BASE UNION FIND
 
-    #TODO LISTA BASE
-    #TODO LISTA CON EURISTICA
-    #TODO ALBERI
-
+# TODO LISTA BASE
+# TODO LISTA CON EURISTICA
+# TODO ALBERI
+from typing import Optional
 
 from RandomGraph import RandomGraph
 import DisjointSetList
 
+
+def printCCResults(graph, result):
+    print(graph.vertices.keys())
+    print(graph.edges)
+
+    for l in result:
+        l.print()
+
+
+def verifySameResults(result1, result2):
+    if len(result1) != len(result2):
+        raise (Exception("Total lengths don't match"))
+    for i in range (0, len(result1)):
+        l1 = result1[i].head
+        l2 = result2[i].head
+        r1 = r2 = []
+        while l1:
+            r1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            r2.append(l2.val)
+            l2 = l2.next
+        if sorted(r1) != sorted(r2):
+            raise (Exception("Arrays don't match"))
+    print("SAME RESULTS!")
+
+
 if __name__ == '__main__':
     n = 10
     rg = RandomGraph(n)
-    #dsl = DisjointSetList.DisjointSetList()
+
+    dsl = DisjointSetList.DisjointSetList()
+    result1 = dsl.connectedComponents(rg)
+
     dsle = DisjointSetList.DisjointSetListHeuristic()
-    # print(rg.vertices.keys())
-    # print(rg.edges)
-    # result = dsl.connectedComponents(rg)
-    # for l in result:
-    #    l.print()
-    print(rg.vertices.keys())
-    print(rg.edges)
-    result = dsle.connectedComponents(rg)
-    for l in result:
-        l.print()
+    result2 = dsle.connectedComponents(rg)
+
+    printCCResults(rg, result1)
+    printCCResults(rg, result2)
+    verifySameResults(result1, result2)
